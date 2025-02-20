4b5faa9fc243fffe95ff025b0da811680add3c6e
Yusef-Mohamed <yoousefmohaamd@gmail.com>
2025-02-20 13:46:08 +0200

    add logos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import { useTranslations } from "next-intl";
 import Logo from "./Logo";
 import { Link } from "@/i18n/routing";
+import Image from "next/image";
+import { appleStoreLink, playStoreLink } from "@/constants";
 
 const Footer = () => {
   const text = useTranslations("footer");
@@ -11,6 +13,33 @@ const Footer = () => {
       <p>
         {text("allRightsReserved")} &copy; {text("lista2025")}{" "}
       </p>
+      {(playStoreLink || appleStoreLink) && (
+        <div className="w-full text-center">
+          <h5 className="mb-4 font-semibold">{text("downloadAppNow")}</h5>
+          <div className="flex items-center justify-center gap-4 max-sm:flex-col">
+            {playStoreLink && (
+              <a href={playStoreLink} target="_blank" rel="noreferrer">
+                <Image
+                  src="/images/google-play.svg"
+                  width={179}
+                  height={52}
+                  alt="googlestore"
+                />
+              </a>
+            )}
+            {appleStoreLink && (
+              <a href={appleStoreLink} target="_blank" rel="noreferrer">
+                <Image
+                  src="/images/app-store.svg"
+                  width={178}
+                  height={52}
+                  alt="appstore"
+                />
+              </a>
+            )}
+          </div>
+        </div>
+      )}
     </footer>
   );
 };
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const appleStoreLink = "https://apps.apple.com/eg";
+export const playStoreLink = "https://play.google.com/store";
