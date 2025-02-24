4955fca27f193bb4953b5df65d206375770aeec2
Yusef-Mohamed <yoousefmohaamd@gmail.com>
2025-02-24 22:27:54 +0200

    added main branch

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const appleStoreLink = "https://apps.apple.com/eg";
-export const playStoreLink = "https://play.google.com/store";
+export const playStoreLink =
+  "https://play.google.com/store/apps/details?id=com.lista.lista";
